dd31ce64641236c6620bfc145fd10cd5bfdac1df
Ademir <ademir.m.junior@gmail.com>
2021-07-31 15:52:47 -0300

    Create first button

--- app/src/main/java/com/example/olamundo/MainActivity.java
+++ app/src/main/java/com/example/olamundo/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.olamundo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextFirstName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+    }
+
+    public void clearFields(View view) {
+        editTextFirstName.setText(null);
     }
 }
