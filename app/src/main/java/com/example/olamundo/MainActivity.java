bcc007735d45b62f8d2fd3dd1646e4a975cc77a9
Ademir <ademir.m.junior@gmail.com>
2021-07-31 17:04:01 -0300

    Develop a full name registration

--- app/src/main/java/com/example/olamundo/MainActivity.java
+++ app/src/main/java/com/example/olamundo/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.olamundo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editTextFirstName;
+    private EditText editTextFirstName, editTextLastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
     }
 
     public void clearFields(View view) {
         editTextFirstName.setText(null);
+        editTextLastName.setText(null);
+
+        editTextFirstName.requestFocus();
     }
+
+    public void showFullName(View view) {
+        String firstName = editTextFirstName.getText().toString();
+        String lastName = editTextLastName.getText().toString();
+
+        Toast.makeText(this, firstName + " " + lastName, Toast.LENGTH_LONG).show();
+    }
+
 }
