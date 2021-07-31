db9fd4e34b9e27c50776bc62f9fc5f3e21c694ed
Ademir <ademir.m.junior@gmail.com>
2021-07-31 17:25:35 -0300

    Validate Input Fields

--- app/src/main/java/com/example/olamundo/MainActivity.java
+++ app/src/main/java/com/example/olamundo/MainActivity.java
@@ -25,13 +25,29 @@ public class MainActivity extends AppCompatActivity {
         editTextLastName.setText(null);
 
         editTextFirstName.requestFocus();
+
+        Toast.makeText(this, R.string.cleared_input_fields, Toast.LENGTH_SHORT).show();
     }
 
     public void showFullName(View view) {
         String firstName = editTextFirstName.getText().toString();
         String lastName = editTextLastName.getText().toString();
 
-        Toast.makeText(this, firstName + " " + lastName, Toast.LENGTH_LONG).show();
+        if (firstName == null || firstName.trim().isEmpty()) {
+            Toast.makeText(this, R.string.first_name_error, Toast.LENGTH_SHORT).show();
+            editTextFirstName.requestFocus();
+            return;
+        }
+
+        if (lastName == null || lastName.trim().isEmpty()) {
+            Toast.makeText(this, R.string.last_name_error, Toast.LENGTH_SHORT).show();
+            editTextLastName.requestFocus();
+            return;
+        }
+
+        Toast.makeText(this,
+                firstName.trim() + " " + lastName.trim(),
+                Toast.LENGTH_LONG).show();
     }
 
 }
